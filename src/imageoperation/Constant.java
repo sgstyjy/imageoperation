00a58feb769a5d1a43781b68423f6a95eb1c9398
sgstyjy <feizhangchina@gmail.com>
2015-09-12 18:18:29 +0200

    test read performance

--- src/imageoperation/Constant.java
+++ src/imageoperation/Constant.java
@@ -2,6 +2,14 @@ package imageoperation;
 
 public class Constant {
 	public static int BUFFER_SIZE = 4*1024;
-	public static String PATH_IN1 = "";
-
+	public static String PATH_IN_PDF = "test.pdf";
+	public static String PATH_IN_MKV = "test.mkv";
+	public static String PATH_IN_ISO = "ubuntu.iso";
+	public static String PATH_IN_TXT = "test.txt";
+	public static String PATH_OUT_PDF = "result.pdf";
+	public static String PATH_OUT_MKV = "result.mkv";
+	public static String PATH_OUT_ISO = "result.iso";
+	public static String PAHT_OUT_TXT = "result.txt";
+	public static String HASHTABLE = "hashtable.txt";
+	public static String COMPHASH = "comparehash.txt";
 }
--- src/imageoperation/ReadImage.java
+++ src/imageoperation/ReadImage.java
@@ -8,34 +8,31 @@ import java.util.Date;
 
 public class ReadImage {
 
-	public static void main(String[] args) throws IOException {
+	public void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		//oldIO
+		//读镜像
+		//BufferedRead reader = new BufferedRead(); //以字符为单位读取，读镜像时会出错
+		MappedRead reader = new MappedRead(); //映射内存
+		//RandomRead reader = new RandomRead(); //随机读，以字节为单位
+		//RandomChannel reader = new RandomChannel(); //随机通道读，以字节为单位
+		//StreamRead reader = new StreamRead();  //输入流读，以字节为单位
+		
+		//读取开始时间
 		Date date = new Date();
 		Long starttime = date.getTime();
 		System.out.println(starttime);
-		oldIOReadFile();
+		
+		//读镜像
+		reader.readFile();
+		
+		//读取结束时间
 		Date date1 = new Date();
 		Long endtime = date1.getTime();
 		System.out.println(endtime);
 		Long duration = endtime-starttime;
 		System.out.println(duration);
+	}
 
-	}
-	public static void oldIOReadFile() throws IOException{
-		BufferedReader br = new BufferedReader(new FileReader("D://Eclipse-workspace/imageoperation/test.pdf"));
-		PrintWriter pw = new PrintWriter("D://Eclipse-workspace/imageoperation/result.pdf");
-		char[] c = new char[4*1024];
-		for(;;){
-			if(br.read(c)!=-1){
-				pw.print(c);
-			}else{
-				break;
-			}
-		}
-		pw.close();
-		br.close();
-	}
 
 
 }
